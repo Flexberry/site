9efa6b3af834af861ca4d03c14ebb31becc368b9
Flexberry-man <mail@flexberry.net>
2018-10-16 13:24:04 +0500

    Update site

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -8,48 +8,4 @@
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
-var cbpAnimatedHeader = (function() {
-
-  var docElem = document.documentElement,
-    header = document.querySelector( '.navbar-fixed-top' ),
-    didScroll = false,
-    changeHeaderOn = 80,
-    pathname = document.location.pathname;
-
-  function init() {
-    window.addEventListener( 'scroll', function( event ) {
-      if( !didScroll ) {
-        didScroll = true;
-        setTimeout( scrollPage, 250 );
-      }
-    }, false );
-  }
-
-  function scrollPage() {
-    var sy = scrollY();
-    if ( sy >= changeHeaderOn) {
-      classie.add( header, 'navbar-shrink' );
-      classie.add( header, 'success' );
-    } else {
-      classie.remove( header, 'navbar-shrink' );
-      classie.remove( header, 'success' );
-    }
-    didScroll = false;
-  }
-
-  function scrollY() {
-    return window.pageYOffset || docElem.scrollTop;
-  }
-
-  if (( pathname.substr(pathname.length - 4) == "/ru/")
-    ||(pathname.includes("/ru/platform-features.html"))
-    ||(pathname.includes("/ru/platform-architecture.html"))
-    ||(pathname.includes("/ru/platform-development-process.html"))
-    ||(pathname.includes("/ru/platform-roadmap.html"))) {
-      init();
-
-  } else {
-    classie.add( header, 'success' );
-    classie.add( header, 'navbar-shrink' );
-  }
-})();
+var cbpAnimatedHeader=function(){function e(){window.addEventListener("scroll",function(e){t||(t=!0,setTimeout(s,250))},!1)}function s(){var e=a();e>=c?(classie.add(r,"navbar-shrink"),classie.add(r,"success")):(classie.remove(r,"navbar-shrink"),classie.remove(r,"success")),t=!1}function a(){return window.pageYOffset||n.scrollTop}var n=document.documentElement,r=document.querySelector(".navbar-fixed-top"),t=!1,c=80,o=document.location.pathname;"/ru/"==o.substr(o.length-4)||o.includes("/ru/platform-features.html")||o.includes("/ru/platform-architecture.html")||o.includes("/ru/platform-development-process.html")||o.includes("/ru/platform-roadmap.html")?e():(classie.add(r,"success"),classie.add(r,"navbar-shrink"))}();
